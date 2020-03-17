9187c15c321baf64ab5300f6e6ac20452f24a344
marcus1676 <marcuscastellanos@gmail.com>
2020-03-17 17:03:54 -0400

    Initial React App and Server backend

---
+++ app/src/test/java/com/spiritsandsuch/app/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiritsandsuch.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
